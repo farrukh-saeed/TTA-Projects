3c5dfa1aed98967219d180df6276a5c656e8add3
Farrukh Saeed <fsaeed1905@users.noreply.github.com>
2021-11-06 08:40:05 +0000

    Add all the new files

---
+++ resturant.py
@@ -0,0 +1,6 @@
+starter = input("Enter your favourite starter in the menu : ")
+meal = input("Enter your main course meal : ")
+dessert = input("Enter your favourite dessert : ")
+drink = input("Enter your drink : ")
+print("Your favourite meal is " + meal, "with starter " + starter, "and dessert is " + dessert, "with a glass of "
+      + drink)
---
+++ studentResults.py
@@ -0,0 +1,11 @@
+marks = input("What are your grades in exams : ")
+if marks >= '90':
+    print(" You got A* grade")
+elif marks >= '80':
+    print("You got A grade ")
+elif marks >= '70':
+    print("You got B grade")
+elif marks >= '60':
+    print(" you got C grade")
+else:
+    print("Unfortunately! you have failed the exam, Have another go")
